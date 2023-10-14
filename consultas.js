11f8572b63fb532bd0727b706214e009f5e0b56a
Juan Pablo Villanueva <juanpablow@gmail.com>
2023-10-14 18:07:14 -0300

    Cambio en post usuarios

--- consultas.js
+++ consultas.js
@@ -1,100 +1,137 @@
-const { Pool } = require('pg')
-const bcrypt = require('bcryptjs')
+const { Pool } = require('pg');
+const bcrypt = require('bcryptjs');
 
 const pool = new Pool({
-    host: 'localhost',
-    user: process.env.PGUSER,
-    password: process.env.PGPASSWORD,
-    database: process.env.PGDATABASE,
-    port: process.env.PGPORT,
-    allowExitOnIdle: true
+  host: 'localhost',
+  user: process.env.PGUSER,
+  password: process.env.PGPASSWORD,
+  database: process.env.PGDATABASE,
+  port: process.env.PGPORT,
+  allowExitOnIdle: true,
 });
 
-
-
-//FUNCION PARA OBTENER LOS USUARIOS 
+//FUNCION PARA OBTENER LOS USUARIOS
 const getUsuarios = async () => {
-    const { rows: usuarios } = await pool.query("SELECT * FROM usuarios")
-    return usuarios;
-}
+  const { rows: usuarios } = await pool.query('SELECT * FROM usuarios');
+  return usuarios;
+};
 
-
-
-// Registro de usuario con clave encriptada// DEBERIAMOS AGREGAR LO QUE ES ROL Y NOMBRE, ESO LO VEREMOS AL HACER LA BASE DE DATOS 
+// Registro de usuario con clave encriptada// DEBERIAMOS AGREGAR LO QUE ES ROL Y NOMBRE, ESO LO VEREMOS AL HACER LA BASE DE DATOS
 const registrarUsuario = async (usuario) => {
-    let { nombre, apellido, correo, clave } = usuario
-    const passwordEncriptada = bcrypt.hashSync(clave)
-    clave = passwordEncriptada
-    const values = [nombre, apellido, correo, passwordEncriptada]
-    const consulta = "INSERT INTO usuarios values (DEFAULT, $1, $2, $3, $4)"
-    await pool.query(consulta, values)
-    }
+  let { name, lastName, email, password } = usuario;
+  const passwordEncriptada = bcrypt.hashSync(password);
+  password = passwordEncriptada;
+  const values = [name, lastName, email, passwordEncriptada];
+  const consulta = 'INSERT INTO usuarios values (DEFAULT, $1, $2, $3, $4)';
+  await pool.query(consulta, values);
+};
 
 //Funcion para inicio de sesion // compareSync hace la comparacion entre la contraseña encriptada vs la contraseña original
 
-const vereficarCredencial = async (correo, clave) =>{
-    const values = [correo]
-    const consulta = "SELECT * FROM usuarios WHERE correo = $1"
-    
-    const {rows: [usuario], rowCount} = await pool.query(consulta, values)
+const vereficarCredencial = async (correo, clave) => {
+  const values = [correo];
+  const consulta = 'SELECT * FROM usuarios WHERE correo = $1';
 
-    const {clave: passwordEncriptada} = usuario
-    const passwordEsCorrecta = bcrypt.compareSync(clave, passwordEncriptada)
-    
-    if(!passwordEsCorrecta || !rowCount)
-    throw { code: 401, message: "Email o contraseña incorrecta" }
-}
+  const {
+    rows: [usuario],
+    rowCount,
+  } = await pool.query(consulta, values);
+
+  const { clave: passwordEncriptada } = usuario;
+  const passwordEsCorrecta = bcrypt.compareSync(clave, passwordEncriptada);
+
+  if (!passwordEsCorrecta || !rowCount)
+    throw { code: 401, message: 'Email o contraseña incorrecta' };
+};
 
 // MODIFICACION DE USUARIOS
 const modificarUsuario = async (nombre, apellido, id) => {
-    const consulta = "UPDATE usuarios SET nombre = $1, apellido = $2 WHERE id = $3"
-    const values = [ nombre, apellido, id]
-    const {rowCount} = await pool.query(consulta, values)
-    if (rowCount === 0) {
-        throw {code: 404, message: "No se consiguio ningun usuario con este id"}
-    }
-}
+  const consulta =
+    'UPDATE usuarios SET nombre = $1, apellido = $2 WHERE id = $3';
+  const values = [nombre, apellido, id];
+  const { rowCount } = await pool.query(consulta, values);
+  if (rowCount === 0) {
+    throw { code: 404, message: 'No se consiguio ningun usuario con este id' };
+  }
+};
 
 //Aregar nuevo producto
-const agregarProducto = async (nombre_producto, precio, url_imagen, descripcion_corta, descripcion) => {
-    const consulta = "INSERT INTO productos values(DEFAULT, $1, $2, $3, $4, $5)"
-    const values = [nombre_producto, precio, url_imagen, descripcion_corta, descripcion]
-    const result = await pool.query(consulta, values)
-    console.log('Producto agregado')
-}
+const agregarProducto = async (
+  nombre_producto,
+  precio,
+  url_imagen,
+  descripcion_corta,
+  descripcion
+) => {
+  const consulta = 'INSERT INTO productos values(DEFAULT, $1, $2, $3, $4, $5)';
+  const values = [
+    nombre_producto,
+    precio,
+    url_imagen,
+    descripcion_corta,
+    descripcion,
+  ];
+  const result = await pool.query(consulta, values);
+  console.log('Producto agregado');
+};
 
-// MODIFICAR UN PRODUCTO 
-const modificarProducto = async (nombre_producto, precio, url_imagen, descripcion_corta, descripcion, id) => {
-    const consulta = "UPDATE productos SET nombre_producto = $1, precio = $2, url_imagen = $3, descripcion_corta = $4, descripcion = $5 WHERE id = $6"
-    const values = [ nombre_producto, precio, url_imagen, descripcion_corta, descripcion, id]
-    const {rowCount} = await pool.query(consulta, values)
-    if (rowCount === 0) {
-        throw {code: 404, message: "No se consiguio ningun viaje con este id"}
-    }
-}
+// MODIFICAR UN PRODUCTO
+const modificarProducto = async (
+  nombre_producto,
+  precio,
+  url_imagen,
+  descripcion_corta,
+  descripcion,
+  id
+) => {
+  const consulta =
+    'UPDATE productos SET nombre_producto = $1, precio = $2, url_imagen = $3, descripcion_corta = $4, descripcion = $5 WHERE id = $6';
+  const values = [
+    nombre_producto,
+    precio,
+    url_imagen,
+    descripcion_corta,
+    descripcion,
+    id,
+  ];
+  const { rowCount } = await pool.query(consulta, values);
+  if (rowCount === 0) {
+    throw { code: 404, message: 'No se consiguio ningun viaje con este id' };
+  }
+};
 
 //ELIMINAR UN PRODUCTO
 const deleteProducto = async (id) => {
-    const consulta = "DELETE FROM productos WHERE id = $1"
-    const values = [id]
-    const { rowCount } = await pool.query(consulta, values)
-    if (!rowCount) throw { code: 404, message: "No se encontró ningún evento con este ID" }
-}
+  const consulta = 'DELETE FROM productos WHERE id = $1';
+  const values = [id];
+  const { rowCount } = await pool.query(consulta, values);
+  if (!rowCount)
+    throw { code: 404, message: 'No se encontró ningún evento con este ID' };
+};
 
-//FUNCION PARA OBTENER LOS PRODUCTOS 
+//FUNCION PARA OBTENER LOS PRODUCTOS
 const getProductos = async () => {
-    const { rows: productos } = await pool.query("SELECT * FROM productos")
-    return productos;
-}
+  const { rows: productos } = await pool.query('SELECT * FROM productos');
+  return productos;
+};
 
 //Funcion para obtener un producto por id
 
 const getProducto = async (id) => {
-    const consulta = "SELECT * FROM productos WHERE id = $1"
-    const value = [id]
-    const { rows: producto } = await pool.query(consulta, value)
-    return producto;
-}
+  const consulta = 'SELECT * FROM productos WHERE id = $1';
+  const value = [id];
+  const { rows: producto } = await pool.query(consulta, value);
+  return producto;
+};
 
-
-module.exports = {registrarUsuario, vereficarCredencial, getProductos, modificarProducto, deleteProducto, agregarProducto, getUsuarios, modificarUsuario, getProducto }
+module.exports = {
+  registrarUsuario,
+  vereficarCredencial,
+  getProductos,
+  modificarProducto,
+  deleteProducto,
+  agregarProducto,
+  getUsuarios,
+  modificarUsuario,
+  getProducto,
+};
--- index.js
+++ index.js
@@ -1,147 +1,173 @@
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const jwt = require("jsonwebtoken")
-const { getProductos, registrarUsuario, vereficarCredencial, modificarProducto, deleteProducto, agregarProducto, getUsuarios, modificarUsuario, getProducto } = require('./consultas')
-const { verificarUsuario, reporte } = require('./middlewares')
-require('dotenv').config()
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const jwt = require('jsonwebtoken');
+const {
+  getProductos,
+  registrarUsuario,
+  vereficarCredencial,
+  modificarProducto,
+  deleteProducto,
+  agregarProducto,
+  getUsuarios,
+  modificarUsuario,
+  getProducto,
+} = require('./consultas');
+const { verificarUsuario, reporte } = require('./middlewares');
+require('dotenv').config();
 
-app.listen(3000, console.log("SERVER ON"))
-app.use(cors())
-app.use(express.json())
+app.listen(3000, console.log('SERVER ON'));
+app.use(cors());
+app.use(express.json());
 
-
-
-
-// Registro de usuario 
+// Registro de usuario
 app.post('/usuarios', reporte, async (req, res) => {
-    try {
-        const usuario = req.body;
-        await registrarUsuario(usuario);
-        res.send("Usuario creado con éxito");
-    } catch (error) {
-        console.error(error);
-        res.status(500).send("Error al crear el usuario: " + error.message);
-    }
+  try {
+    const usuario = req.body;
+    console.log(usuario);
+    await registrarUsuario(usuario);
+    res.send('Usuario creado con éxito');
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Error al crear el usuario: ' + error.message);
+  }
 });
 
-// INICIO DE SESION USUARIO YA REGISTRADO // jwt.sign genera un token, primer argumento es un payload y llave secreta se usa para decodificar el token 
+// INICIO DE SESION USUARIO YA REGISTRADO // jwt.sign genera un token, primer argumento es un payload y llave secreta se usa para decodificar el token
 app.post('/login', verificarUsuario, reporte, async (req, res) => {
-    try {
-        const { correo, clave } = req.body
-        await vereficarCredencial(correo, clave)
-        const token = jwt.sign({ correo }, "Llave_secreta")
-        res.send(token)
-    } catch (error) {
-        console.log(error)
-        res.status(error.code || 500).send(error)
-    }
+  try {
+    const { correo, clave } = req.body;
+    await vereficarCredencial(correo, clave);
+    const token = jwt.sign({ correo }, 'Llave_secreta');
+    res.send(token);
+  } catch (error) {
+    console.log(error);
+    res.status(error.code || 500).send(error);
+  }
+});
 
-})
-
-//  RUTA GET PARA VISUALIZAR LOS USUARIOS 
+//  RUTA GET PARA VISUALIZAR LOS USUARIOS
 app.get('/usuarios', reporte, async (req, res) => {
-    try {
-        const usuarios = await getUsuarios()
-        res.json(usuarios)
-    } catch (error) {
-        res.status(error.code || 500).send(error)
-    }
-})
-
+  try {
+    const usuarios = await getUsuarios();
+    res.json(usuarios);
+  } catch (error) {
+    res.status(error.code || 500).send(error);
+  }
+});
 
 app.put('/usuarios/:id', reporte, async (req, res) => {
-
-    const { id } = req.params
-    const { nombre, apellido } = req.query  // aqui colocaremos lo que queremos modificar del producto 
-    const Authorization = req.header('Authorization') // Da la autorizacion al token para poder hacer la modificacion
-    const token = Authorization.split('Bearer ')[1]
-    jwt.verify(token, "Llave_secreta")  // Verifica el token y le da el ok 
-    const { correo } = jwt.decode(token)  // decodifica el token para ver la informacion que posee
-    try {
-        await modificarUsuario(nombre, apellido, id)
-        res.send(`El usuario ${correo} ha modificado el usuario de id ${id}`)
-    } catch (error) {
-        res.status(error.code || 500).send(error)
-    }
-})
-
+  const { id } = req.params;
+  const { nombre, apellido } = req.query; // aqui colocaremos lo que queremos modificar del producto
+  const Authorization = req.header('Authorization'); // Da la autorizacion al token para poder hacer la modificacion
+  const token = Authorization.split('Bearer ')[1];
+  jwt.verify(token, 'Llave_secreta'); // Verifica el token y le da el ok
+  const { correo } = jwt.decode(token); // decodifica el token para ver la informacion que posee
+  try {
+    await modificarUsuario(nombre, apellido, id);
+    res.send(`El usuario ${correo} ha modificado el usuario de id ${id}`);
+  } catch (error) {
+    res.status(error.code || 500).send(error);
+  }
+});
 
 // Agregar un producto
 app.post('/productos', reporte, async (req, res) => {
-    try {
-        const { nombre_producto, precio, url_imagen, descripcion_corta, descripcion } = req.body
-        await agregarProducto(nombre_producto, precio, url_imagen, descripcion_corta, descripcion)
-        res.send("Producto agregado con éxito")
-    } catch (error) {
-        const { code } = error
-        if (code == "23502") {
-            res.status(400)
-                .send("Se ha violado la restricción NOT NULL en uno de los campos de la tabla")
-        } else {
-            res.status(500).send(error)
-        }
+  try {
+    const {
+      nombre_producto,
+      precio,
+      url_imagen,
+      descripcion_corta,
+      descripcion,
+    } = req.body;
+    await agregarProducto(
+      nombre_producto,
+      precio,
+      url_imagen,
+      descripcion_corta,
+      descripcion
+    );
+    res.send('Producto agregado con éxito');
+  } catch (error) {
+    const { code } = error;
+    if (code == '23502') {
+      res
+        .status(400)
+        .send(
+          'Se ha violado la restricción NOT NULL en uno de los campos de la tabla'
+        );
+    } else {
+      res.status(500).send(error);
     }
-})
+  }
+});
 
-
-//  RUTA GET PARA VISUALIZAR LOS PRODUCTOS 
-app.get("/productos", reporte, async (req, res) => {
-    try {
-        const productos = await getProductos()
-        res.json(productos)
-    } catch (error) {
-        res.status(error.code || 500).send(error)
-    }
-})
-
+//  RUTA GET PARA VISUALIZAR LOS PRODUCTOS
+app.get('/productos', reporte, async (req, res) => {
+  try {
+    const productos = await getProductos();
+    res.json(productos);
+  } catch (error) {
+    res.status(error.code || 500).send(error);
+  }
+});
 
 // Ruta GET productos/:id
 app.get('/productos/:id', reporte, async (req, res) => {
-    try {
-        const { id } = req.params
-        const producto = await getProducto(id)
-        res.json(producto)
-    } catch (error) {
-        res.status(error.code || 500).send(error)
-    }
+  try {
+    const { id } = req.params;
+    const producto = await getProducto(id);
+    res.json(producto);
+  } catch (error) {
+    res.status(error.code || 500).send(error);
+  }
+});
 
-
-})
-
-
-
-//Modificacion de producto con autorizacion de token 
+//Modificacion de producto con autorizacion de token
 app.put('/productos/:id', reporte, async (req, res) => {
-    try {
-        const { id } = req.params
-        const { nombre_producto, precio, url_imagen, descripcion_corta, descripcion } = req.query  // aqui colocaremos lo que queremos modificar del producto 
-        const Authorization = req.header('Authorization') // Da la autorizacion al token para poder hacer la modificacion
-        const token = Authorization.split('Bearer ')[1]
-        jwt.verify(token, "Llave_secreta")  // Verifica el token y le da el ok 
-        const { correo } = jwt.decode(token)  // decodifica el token para ver la informacion que posee
-        await modificarProducto(nombre_producto, precio, url_imagen, descripcion_corta, descripcion, id)
-        res.send(`El usuario ${correo} ha modificado el producto de id ${id}`)
-    } catch (error) {
-        res.status(error.code || 500).send(error)
-    }
+  try {
+    const { id } = req.params;
+    const {
+      nombre_producto,
+      precio,
+      url_imagen,
+      descripcion_corta,
+      descripcion,
+    } = req.query; // aqui colocaremos lo que queremos modificar del producto
+    const Authorization = req.header('Authorization'); // Da la autorizacion al token para poder hacer la modificacion
+    const token = Authorization.split('Bearer ')[1];
+    jwt.verify(token, 'Llave_secreta'); // Verifica el token y le da el ok
+    const { correo } = jwt.decode(token); // decodifica el token para ver la informacion que posee
+    await modificarProducto(
+      nombre_producto,
+      precio,
+      url_imagen,
+      descripcion_corta,
+      descripcion,
+      id
+    );
+    res.send(`El usuario ${correo} ha modificado el producto de id ${id}`);
+  } catch (error) {
+    res.status(error.code || 500).send(error);
+  }
+});
 
-})
-
-// Borrar producto con autorizacion de token 
+// Borrar producto con autorizacion de token
 app.delete('/productos/:id', reporte, async (req, res) => {
-    try {
-        const { id } = req.params
-        const Authorization = req.header('Authorization')
-        const token = Authorization.split('Bearer ')[1]
-        jwt.verify(token, "Llave_secreta")
-        const { correo } = jwt.decode(token)
-        await deleteProducto(id)
-        res.send(`El usuario ${correo} ha eliminado el producto de id ${id}`)
+  try {
+    const { id } = req.params;
+    const Authorization = req.header('Authorization');
+    const token = Authorization.split('Bearer ')[1];
+    jwt.verify(token, 'Llave_secreta');
+    const { correo } = jwt.decode(token);
+    await deleteProducto(id);
+    res.send(`El usuario ${correo} ha eliminado el producto de id ${id}`);
+  } catch (error) {
+    res
+      .status(404)
+      .send({ message: 'No se encontró ningún producto con ese id' });
+  }
+});
 
-    } catch (error) {
-        res.status(404).send({ message: "No se encontró ningún producto con ese id" })
-    }
-})
-
-module.exports = app
+module.exports = app;
